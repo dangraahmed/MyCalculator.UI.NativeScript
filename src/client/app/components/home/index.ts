7565bfbf70b0863219eb410ea951c51a6f76b577
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2017-09-05 16:08:59 +0530

    Structure change

---
+++ src/client/app/components/home/index.ts
@@ -0,0 +1,7 @@
+import { HomeComponent } from './home.component';
+import { AboutComponent } from '../about/about.component';
+
+export const HOME_COMPONENTS: any[] = [
+    HomeComponent,
+    AboutComponent
+];
--- src/client/app/components/index.ts
+++ src/client/app/components/index.ts
@@ -1,21 +1,16 @@
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { AdminComponent } from './admin/admin.component';
+import { HOME_COMPONENTS } from './home/index';
+import { ADMIN_COMPONENTS } from './admin/index';
 import { ACCOUNT_COMPONENTS } from './account/index';
-import { TAX_SLAB_COMPONENTS } from './taxSlab/index';
-
 
 export const APP_COMPONENTS: any[] = [
   AppComponent,
-  AboutComponent,
-  HomeComponent,
-  AdminComponent,
-  ...ACCOUNT_COMPONENTS,
-  ...TAX_SLAB_COMPONENTS
+  ...HOME_COMPONENTS,
+  ...ADMIN_COMPONENTS,
+  ...ACCOUNT_COMPONENTS
 ];
 
 export * from './app.component';
-export * from './about/about.component';
 export * from './home/home.component';
 export * from './admin/admin.component';
+export * from './account/index';
