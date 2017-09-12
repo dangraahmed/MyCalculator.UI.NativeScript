5179b22ec8ed597ccc3b5926135aa65095937840
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2017-09-12 18:22:21 +0530

    data loading added.

--- src/client/app/modules/taxSlab/services/taxSlab.service.ts
+++ src/client/app/modules/taxSlab/services/taxSlab.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ApiService } from '../../shared/provider/api.service';
-import { ITaxSlab } from '../states/taxSlab.state';
+import { ITaxSlab, ITaxSlabState } from '../states/taxSlab.state';
 
 @Injectable()
 export class TaxSlabService {
@@ -11,26 +11,26 @@ export class TaxSlabService {
     }
 
     getTaxSlabs(): Observable<Array<ITaxSlab>> {
-        let dummyTaxSlab: ITaxSlab[] = [
-            {
-                id: 1,
-                fromYear: 2001,
-                toYear: 2002,
-                category: 'A'
+        // let dummyTaxSlab: ITaxSlab[] = [
+        //     {
+        //         id: 1,
+        //         fromYear: 2001,
+        //         toYear: 2002,
+        //         category: 'A'
 
-            }, {
-                id: 2,
-                fromYear: 2002,
-                toYear: 2003,
-                category: 'B'
+        //     }, {
+        //         id: 2,
+        //         fromYear: 2002,
+        //         toYear: 2003,
+        //         category: 'B'
 
-            }
-        ];
+        //     }
+        // ];
 
-        return Observable.of(dummyTaxSlab);
-        // return this.http.get({}, 'taxSlab/listTaxSlabs')
-        //     .map(res => res)
-        //     .catch(this.handleError.bind(this));
+        // return Observable.of(dummyTaxSlab);
+        return this.http.get({}, 'taxSlab/listTaxSlabs')
+            .map(res => res)
+            .catch(this.handleError.bind(this));
     }
 
     private handleError(error: any): Observable<any> {
